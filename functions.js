4093799b97324070358757214046a4d0a33ed106
Jason E. Church <elijahchurch@gmail.com>
2023-09-01 10:36:18 -0700

    Solve URL formatter problem with and without recursion

---
+++ functions.js
@@ -0,0 +1,28 @@
+
+//URL replacer with recursion
+function urlReplacer(string) {
+    if (string === "") {
+        return "";
+    }
+    else {
+        let firstLetter = string[0];
+        console.log(firstLetter);
+        if(firstLetter === " ") {
+            firstLetter = "%20";
+        }
+        return firstLetter + urlReplacer(string.substring(1));
+    }
+};
+
+//URL replacer without recursion
+function urlReplacer2(string) {
+    let newString = "";
+    for(let i = 0; i < string.length; i ++) {
+        let chara = string[i];
+        if (chara === " ") {
+            chara = "%20";
+        }
+        newString += chara;
+    }
+    return newString
+};
